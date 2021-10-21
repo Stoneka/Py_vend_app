89d51b96bac3ca8453eaab3262b165dbef5eeba5
Stoneka <88184040+Stoneka@users.noreply.github.com>
2021-10-21 17:53:11 +0900

    drinkモデルの作成

---
+++ models/drink.py
@@ -0,0 +1,18 @@
+#coding: UTF-8
+class Drink(object):
+    def __init__(self, name, fee):
+        self.name = name
+        self.fee = fee
+
+def drink_registration():
+    print("商品を登録してください")
+    drinks = {}
+    i = 0
+    while i < 3:
+        drink_name = input("商品名を入力してください")
+        drink_fee = input("商品の金額を入力してください")
+        drink = Drink(drink_name, drink_fee)
+        drinks.update({drink.name : drink.fee})
+        i += 1
+    print(drinks)
+drink_registration()
