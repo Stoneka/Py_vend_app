f062274dc1a96930392426bad98bcb4da65a8ddb
Stoneka <88184040+Stoneka@users.noreply.github.com>
2021-10-21 23:15:02 +0900

    決済処理機能追加

--- models/user.py
+++ models/user.py
@@ -1,9 +1,11 @@
 class User(object):
     def __init__(self,money):
         self.money = money
-
+    def choose_drink(self):
+        chosen_drink = input("商品番号を選んでください")
 def put_money():
     money = input("お金を入れてください")
     user = User(money)
     print("{}円入っています".format(user.money))
+    user.choose_drink()
 put_money()
--- models/vending_machine.py
+++ models/vending_machine.py
@@ -1,11 +1,27 @@
 class VendingMachine(object):
     def __init__(self, drinks):
         self.drinks = drinks
+
     def show_drinks(self):
         print("いらっしゃいませ。以下商品を販売しています")
-        for name,fee in self.drinks.items():
-            print("【{} 】{}円".format(name, fee))
-drinks = {'apple': '100', 'banana': '110', 'orange': '120'}
+        num = 1
+        for item in self.drinks:
+            for name,fee in item.items():
+                print("【{}】{}:{}円".format(num, name, fee))
+                num += 1
+
+    def pay_money(self):
+        chosen_drink = 1
+        money = 1000
+        item_num = self.drinks[chosen_drink - 1]
+        for i in item_num.values():
+            price = int(i)
+        change = money - price
+        print("お買い上げありがとうございます、お釣りは{}円です".format(change))
+
+
+drinks = [{'apple': '100'}, {'banana': '110'}, {'orange': '120'}]
 #drinksは、後ほどDrink_modelから入力する
 vending = VendingMachine(drinks)
 vending.show_drinks()
+vending.pay_money()
