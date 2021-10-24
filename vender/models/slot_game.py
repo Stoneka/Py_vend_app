f893a393cc1adbc5bd015692d27fd269d043640a
Stoneka <88184040+Stoneka@users.noreply.github.com>
2021-10-24 13:11:41 +0900

    slot_gameモデル追加

---
+++ vender/models/slot_game.py
@@ -0,0 +1,19 @@
+import random
+
+class SlotGame(object):
+    def play_slot(self):
+        result = []
+        i = 1
+        while i < 4:
+            result.append(random.randint(0, 9))
+            i += 1
+        result = map(str, result)
+        result = "".join(result)
+        print("スロットの結果は{}です".format(result))
+        if result[0] == result[1] and result[0] == result[2]:
+            print("おめでとう！アタリです")
+        else:
+            print("残念･･･ハズレです")
+start_slot = SlotGame()
+start_slot.play_slot()
+
