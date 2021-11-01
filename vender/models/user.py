64de15a500de48784c06c9944b0c4101cdd69519
Stoneka <88184040+Stoneka@users.noreply.github.com>
2021-11-01 23:17:54 +0900

    お金の投入機能に整数でしか投入できない制限を加える

--- vender/models/user.py
+++ vender/models/user.py
@@ -5,7 +5,15 @@ class User(object):
         chosen_drink = input("商品番号を選んでください")
         return chosen_drink
 def put_money():
-    money = input("お金を入れてください")
+    money = check_integer()
     user = User(money)
     print("{}円入っています".format(user.money))
     return user
+def check_integer():
+    money = input("お金を入れてください")
+    try:
+        money = int(money)
+    except:
+        print("整数で入力してください")
+        return check_integer()
+    return money
