99afd14dc3a455eea3a03f134d3def816a2375f2
Stoneka <88184040+Stoneka@users.noreply.github.com>
2021-11-02 21:24:54 +0900

    商品番号入力機能に一部制限を加える

--- vender/models/user.py
+++ vender/models/user.py
@@ -2,18 +2,30 @@ class User(object):
     def __init__(self,money):
         self.money = money
     def choose_drink(self):
-        chosen_drink = input("商品番号を選んでください")
+        chosen_drink = check_drink_integer()
         return chosen_drink
 def put_money():
-    money = check_integer()
+    money = check_money_integer()
     user = User(money)
     print("{}円入っています".format(user.money))
     return user
-def check_integer():
+def check_money_integer():
     money = input("お金を入れてください")
     try:
         money = int(money)
     except:
         print("整数で入力してください")
-        return check_integer()
+        return check_money_integer()
     return money
+def check_drink_integer():
+    chosen_drink = input("商品番号を選んでください")
+    try:
+        chosen_drink = int(chosen_drink)
+    except:
+        print("整数で入力してください")
+        return check_drink_integer()
+    if chosen_drink < 0 or chosen_drink > 3:
+        print("１〜３の数字で入力してください")
+        return check_drink_integer()
+    else:
+        return chosen_drink
