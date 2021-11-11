2f6861b980f9120034782d77ca22d62505652421
Stoneka <88184040+Stoneka@users.noreply.github.com>
2021-11-11 23:48:28 +0900

    商品の購入処理の更新

--- vender/models/user.py
+++ vender/models/user.py
@@ -1,8 +1,8 @@
 class User(object):
     def __init__(self,money):
         self.money = money
-    def choose_drink(self):
-        chosen_drink = check_drink_integer()
+    def choose_drink(self, drinks):
+        chosen_drink = check_drink_integer(drinks)
         return chosen_drink
 def put_money():
     money = check_money_integer()
@@ -17,15 +17,15 @@ def check_money_integer():
         print("整数で入力してください")
         return check_money_integer()
     return money
-def check_drink_integer():
+def check_drink_integer(drinks):
     chosen_drink = input("商品番号を選んでください")
     try:
         chosen_drink = int(chosen_drink)
     except:
         print("整数で入力してください")
-        return check_drink_integer()
-    if chosen_drink < 0 or chosen_drink > 3:
-        print("１〜３の数字で入力してください")
-        return check_drink_integer()
+        return check_drink_integer(drinks)
+    if chosen_drink < 0 or chosen_drink > len(drinks):
+        print("１〜{}の数字で入力してください".format(len(drinks)))
+        return check_drink_integer(drinks)
     else:
         return chosen_drink
