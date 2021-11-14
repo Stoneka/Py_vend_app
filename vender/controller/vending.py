f5ce1f2e1b0314714b7e93889a2dfe45877a029a
Stoneka <88184040+Stoneka@users.noreply.github.com>
2021-11-14 23:34:31 +0900

    プログラムの継続、終了を選べる機能を追加

--- vender/controller/vending.py
+++ vender/controller/vending.py
@@ -7,8 +7,32 @@ def start_vending():
     drinks = drink.drink_registration()
     machine = vending_machine.VendingMachine(drinks)
     machine.show_drinks()
+    edit_vending(drinks, machine)
+    #drink.edit_drink(drinks)
+    #machine.show_drinks()
+    #purchaser = user.put_money()
+    #chosen = purchaser.choose_drink(drinks)
+    #machine.pay_money(purchaser.money, chosen)
+#商品編集機能を切り出し
+def edit_vending(drinks, machine):
     drink.edit_drink(drinks)
     machine.show_drinks()
     purchaser = user.put_money()
     chosen = purchaser.choose_drink(drinks)
     machine.pay_money(purchaser.money, chosen)
+    choice_vending(drinks, machine)
+#プログラムを継続、終了を選ぶ機能
+def choice_vending(drinks, machine):
+    choice = input("購入を続けますか？数字を入力してください。【１】続ける【２】終了する")
+    try:
+        choice = int(choice)
+    except:
+        print("整数で入力してください")
+        return choice_vending(drinks, machine)
+    if choice == 1:
+        return edit_vending(drinks, machine)
+    elif choice == 2:
+        return exit()
+    else:
+        print("1~2で入力してください")
+        return choice_vending(drinks, machine)
